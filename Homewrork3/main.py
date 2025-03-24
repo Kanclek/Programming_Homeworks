09c544d65acc2daa3d7725bf5e30409e533dece5
Kanclek <Kanclek@gmail.com>
2025-03-24 15:08:21 +0300

    Finish: complete homework 3

--- Homewrork3/main.py
+++ Homewrork3/main.py
@@ -1,9 +1,36 @@
 from sympy import sympify
 
+class Calculator:
+
+    def __init__(self, text):
+        '''Принимает стоку-выражение str и вызывает вычисления'''
+        self.__text = text
+        self.__result = self.calculate()
+        if round(self.__result) == self.__result:
+            self.__result = int(self.__result)
+
+        print(text, " = ", self.__result)
+
+    def calculate(self):
+        '''Вычисление значения, возвращает float'''
+        expr = sympify(self.__text)
+        result = expr.evalf(2)
+        return float(result)
+
+    def __doc__(self):
+        text = '''
+        Принимает стоку-выражение типа:
+         A оператор B
+         или для унарных операторов
+         оператор(A)
+         
+         Вычисляет значение
+        '''
+        return text
+
+
 text = input("Напишите пример:")
-expr = sympify(text)
-result = expr.evalf(2)
-print(float(result))
 
-
+instance = Calculator(text)
+print(instance.__doc__())
 
